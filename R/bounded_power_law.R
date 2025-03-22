a742343366b29e682ac166c36a81efa92147a42b
Alexis Lazaris <alexislazr@outlook.com>
2025-03-22 09:15:44 +0200

    added binned data functionality in `bPL`

--- R/bounded_power_law.R
+++ R/bounded_power_law.R
@@ -1,8 +1,8 @@
 #' Individual Size Distribution for fitting a GAMLSS
 #'
 #' `bPL` returns a distribution function for ISD described as a bounded Power-Law distribution (bPL) in
-#' Edwards et al. 2017. `bPL` is defined by only one parameter (defined as `mu` here) which is equivalent to 'b'
-#' as described in Edwards et al. 2017. When fitting with `gamlss2`, estimate for `mu` is returned.
+#' Edwards et al. 2017. `bPL` is defined by only one parameter (defined as `b` here) which is equivalent to 'b'
+#' as described in Edwards et al. 2017. When fitting with `gamlss2`, an estimate for `b` is returned.
 #'
 #' @param counts Number of observations for a corresponding size
 #' @param lower Lowest size limit (does not need to equal `min(size)`)
@@ -14,18 +14,35 @@
 #'
 #' @examples
 #'
+#' # For these examples we need the `sizeSpetra` package.
+#' # remotes::install_github("andrew-edwards/sizeSpectra")
+#' # Note, though, that `sizeSpectra` is not needed for `gamlss2.ISD` in general.
+#'
 #' # simulate a dataset of 1000 sizes coming from an ISD with
 #' # b = -2, and range of sizes bounded between 1 and 1000
 #'
 #' # 1) either use sizeSpectra::rPLB
 #' d = data.frame(size = sizeSpectra::rPLB(1e4, -2, 1, 1000))
-#' # 2) or, alternatively, use `bPL()$r`
-#' d = data.frame(size = bPL(1, 1, 1000)$r(1e4, list(mu = -2)))
+#' # 2) or, alternatively, use `bPL()$r` (by accessing the `$r` function of `bPL`)
+#' d = data.frame(size = bPL(1, 1, 1000)$r(1e4, list(b = -2)))
 #'
 #' # estimate the exponent 'b'
-#' m = gamlss2(size ~ 1, data = d, family = bPL(counts = 1, lower = 1, upper = 1000))
+#' m = gamlss2(size ~ 1,
+#'             data = d,
+#'             family = bPL(counts = 1, lower = 1, upper = 1000))
 #'
 #' summary(m)
+#'
+#' # If data is in binned form; i.e.:
+#' bin.d = sizeSpectra::binData(d$size, binWidth = "2k")$binVals
+#'
+#' # We can, also, use a 2-column matrix for the response:
+#' bin.m = gamlss2(cbind(binMin, binMax) ~ 1,
+#'                 data = bin.d,
+#'                 family = bPL(counts = bin.d$binCount, lower = min(bin.d$binMin), upper = max(bin.d$binMax)))
+#'
+#'
+#' summary(bin.m)
 #'
 #' @references Edwards, A.M., Robinson, J.P.W., Plank, M.J., Baum, J.K. and Blanchard, J.L. (2017), Testing and recommending methods for fitting size spectra to data. Methods Ecol Evol, 8: 57-67. https://doi.org/10.1111/2041-210X.12641
 #' @references Edwards, A. M., Robinson, J. P. W., Blanchard, J. L., Baum, J. K., & Plank, M. J. (2020). Accounting for the bin structure of data removes bias when fitting size spectra. Marine Ecology Progress Series, 636, 19–33. https://www.jstor.org/stable/26920653
@@ -35,50 +52,120 @@ bPL = function(counts, lower, upper)
 {
   fam = list(
     "family" = "bPL",
-    "names" = "mu",
-    "links" = c("mu" = "identity"),
+    "names" = "b",
+    "links" = c("b" = "identity"),
     "score" = list(
-      "mu" = function(y, par, ...) {
-        .Call("score_bpl", as.numeric(y), as.numeric(par$mu), as.numeric(counts), as.numeric(lower), as.numeric(upper))
+      "b" = function(y, par, ...) {
+        if(NCOL(y) == 1) {
+          .Call("score_bpl",
+                as.numeric(y),
+                as.numeric(par$b),
+                as.numeric(counts),
+                as.numeric(lower), as.numeric(upper))
+        } else {
+          .Call("score_bpl_binned",
+                as.numeric(y[, 1]), as.numeric(y[, 2]),
+                as.numeric(par$b),
+                as.numeric(counts),
+                as.numeric(lower), as.numeric(upper))
+        }
       }
     ),
     "hess" = list(
-      "mu" = function(y, par, ...) {
-        .Call("hess_bpl", as.numeric(y), as.numeric(par$mu), as.numeric(counts), as.numeric(lower), as.numeric(upper))
+      "b" = function(y, par, ...) {
+        if(NCOL(y) == 1) {
+          .Call("hess_bpl",
+                as.numeric(y),
+                as.numeric(par$b),
+                as.numeric(counts),
+                as.numeric(lower), as.numeric(upper))
+        } else {
+          .Call("hess_bpl_binned",
+                as.numeric(y[, 1]), as.numeric(y[, 2]),
+                as.numeric(par$b),
+                as.numeric(counts),
+                as.numeric(lower), as.numeric(upper))
+        }
       }
     ),
     "loglik" = function(y, par, ...) {
-      sum(.Call("loglik_bpl", as.numeric(y), as.numeric(par$mu), as.numeric(counts), as.numeric(lower), as.numeric(upper)))
+      if(NCOL(y) == 1) {
+        sum(.Call("loglik_bpl",
+                  as.numeric(y),
+                  as.numeric(par$b),
+                  as.numeric(counts),
+                  as.numeric(lower), as.numeric(upper)))
+      } else {
+        sum(.Call("loglik_bpl_binned",
+                  as.numeric(y[, 1]), as.numeric(y[, 2]),
+                  as.numeric(par$b),
+                  as.numeric(counts),
+                  as.numeric(lower), as.numeric(upper)))
+      }
     },
-    "mu" = function(par, ...) {
-      par$mu
+    "b" = function(par, ...) {
+      par$b
     },
     "d" = function(y, par, log = FALSE) {
-      .Call("d_bpl", as.numeric(y), as.numeric(par$mu), as.numeric(lower), as.numeric(upper))
+      stop("used 'd'")
+      if(NCOL(y) == 2) y = y[, 1] #y = (y[, 1] + y[, 2]) / 2
+      .Call("d_bpl",
+            as.numeric(y),
+            as.numeric(par$b),
+            as.numeric(lower), as.numeric(upper))
     },
     "p" = function(y, par, ...) {
-      .Call("p_bpl", as.numeric(y), as.numeric(par$mu), as.numeric(lower), as.numeric(upper))
+      if(NCOL(y) == 1) {
+        .Call("p_bpl",
+              as.numeric(y),
+              as.numeric(par$b),
+              as.numeric(lower), as.numeric(upper))
+      } else {
+        .Call("p_bpl_binned",
+              as.numeric(y[, 1]), as.numeric(y[, 2]),
+              as.numeric(par$b),
+              as.numeric(lower), as.numeric(upper))
+      }
     },
     "r" = function(n, par) {
       rand = stats::runif(n)
-      .Call("r_bpl", as.integer(n), as.numeric(par$mu), as.numeric(lower), as.numeric(upper), as.numeric(rand))
+      .Call("r_bpl",
+            as.integer(n),
+            as.numeric(par$b),
+            as.numeric(lower), as.numeric(upper),
+            as.numeric(rand))
     },
     "q" = function(p, par) {
-      .Call("q_bpl", as.numeric(p), as.numeric(par$mu), as.numeric(lower), as.numeric(upper))
+      .Call("q_bpl",
+            as.numeric(p),
+            as.numeric(par$b),
+            as.numeric(lower), as.numeric(upper))
     },
     "initialize" = list(
-      "mu" = function(y, ...) {
-        .Call("init_bpl", as.numeric(y), as.numeric(counts), as.numeric(lower), as.numeric(upper))
+      "b" = function(y, ...) {
+        if(NCOL(y) == 1) {
+          .Call("init_bpl",
+                as.numeric(y),
+                as.numeric(counts),
+                as.numeric(lower), as.numeric(upper))
+        } else {
+          .Call("init_bpl_binned",
+                as.numeric(y[, 1]), as.numeric(y[, 2]),
+                as.numeric(counts),
+                as.numeric(lower), as.numeric(upper))
+        }
       }
     ),
-    "mean" = function(par) par$mu,
+    "mean" = function(par) par$b,
     "valid.response" = function(x) {
+      #if(NCOL(x) == 1) cat("`bPL`: response is individual sizes (1-d)\n") else cat("`bPL`: response is binned (2-d)\n")
       if(!is.numeric(x)) stop("the response should be numeric")
-      if(any(x == 0)) stop("response equal to 0 detected")
+      #if(any(x == 0)) stop("response equal to 0 detected")
+      if(anyNA(x)) stop("`NA`s in response; better to pass a dataset without `NA`s to 'gamlss2'")
       if((!length(counts)) || (!length(lower)) || (!length(upper)) || anyNA(counts) || anyNA(lower) || anyNA(upper)) stop("invalid 'counts'/'lower'/'upper'")
       if(any(lower > upper)) stop("need lower < upper")
       if(any((x < lower) | (x > upper))) stop("response should be in [lower, upper]")
-      if(any(counts == 0)) stop("0 counts detected")
+      #if(any(counts == 0)) stop("0 counts detected")
       return(TRUE)
     }
   )
--- src/bounded_power_law.c
+++ src/bounded_power_law.c
@@ -1,6 +1,7 @@
 #include <R.h>
 #include <Rdefines.h>
 #include <Rinternals.h>
+#include <Rmath.h>
 
 
 // macros to find max; used to find 'max(length(a), length(b), ...)' to emulate recycling behaviour
@@ -11,26 +12,93 @@
 
 
 
-// PDF of ISD; adapted from 'isdbayes' package
-SEXP d_bpl(SEXP y, SEXP mu, SEXP l, SEXP u)
+// adapted from Edwards et al. 2017 (sizeSpectra::negLL.PLB.counts)
+SEXP loglik_bpl(SEXP y, SEXP b, SEXP c, SEXP l, SEXP u)
 {
-  int ny = LENGTH(y), nmu = LENGTH(mu), nl = LENGTH(l), nu = LENGTH(u);
-  int N = MAX4(ny, nmu, nl, nu);
+  int ny = LENGTH(y), nb = LENGTH(b), nc = LENGTH(c), nl = LENGTH(l), nu = LENGTH(u);
+  int N = MAX5(ny, nb, nc, nl, nu);
 
   SEXP ans;
   PROTECT(ans = Rf_allocVector(REALSXP, N));
 
-  double *py = REAL(y), *pmu = REAL(mu), *pl = REAL(l), *pu = REAL(u), *pans = REAL(ans);
+  double *py = REAL(y), *pc = REAL(c), *pb = REAL(b), *pl = REAL(l), *pu = REAL(u), *pans = REAL(ans);
 
   for(int i = 0; i < N; i++) {
-    if((py[i % ny] < pl[i % nl]) || (py[i % ny] > pu[i % nu])) {
-      pans[i] = 0.0;
-      continue;
-    }
-    if(pmu[i % nmu] != -1) {
-      pans[i] = (pmu[i % nmu] + 1) * pow(py[i % ny], (pmu[i % nmu] + 1)) / ( pow(pu[i % nu], (pmu[i % nmu] + 1)) - pow(pl[i % nl], (pmu[i % nmu] + 1)) );
+    if(pb[i % nb] != -1) {
+      pans[i] = pc[i % nc] * log((pb[i % nb] + 1)/(R_pow(pu[i % nu], pb[i % nb] + 1) - R_pow(pl[i % nl], pb[i % nb] + 1))) + pb[i % nb] * pc[i % nc] * log(py[i % ny]);
     } else {
-      pans[i] = pow(py[i % ny], -2)/(pl[i % nl] * log(pu[i % nu]/pl[i % nl]));
+      pans[i] = -pc[i % nc] * log(log(pu[i % nu]) - log(pl[i % nl])) - pc[i % nc] * log(py[i % ny]);
+      //pans[i] = pc[i % nc] * (log(log(pl[i % nl]) - log(pu[i % nu])) + pb[i % nb] * log(py[i % ny]));
+    }
+  }
+
+  UNPROTECT(1);
+  return(ans);
+}
+
+// adapted from sizeSpectra::negLL.PLB.bins.species
+SEXP loglik_bpl_binned(SEXP y1, SEXP y2, SEXP b, SEXP c, SEXP l, SEXP u)
+{
+  int ny = LENGTH(y1), nb = LENGTH(b), nc = LENGTH(c), nl = LENGTH(l), nu = LENGTH(u);
+  int N = MAX5(ny, nb, nc, nl, nu);
+
+  SEXP ans;
+  PROTECT(ans = Rf_allocVector(REALSXP, N));
+
+  double *py1 = REAL(y1), *py2 = REAL(y2), *pb = REAL(b), *pc = REAL(c), *pl = REAL(l), *pu = REAL(u), *pans = REAL(ans);
+  double comp2, comp1;
+
+  // adapted from https://github.com/andrew-edwards/sizeSpectra/blob/master/R/likelihood.R#L443
+
+  for(int i = 0; i < N; i++) {
+    if(pb[i % nb] != -1) {
+      comp2 = pc[i % nc] * log( fabs( R_pow(py2[i % ny], pb[i % nb] + 1) - R_pow(py1[i % ny],  pb[i % nb] + 1) ) );
+      comp1 = - pc[i % nc] * log( fabs( R_pow(pu[i % nu], pb[i % nb] + 1) - R_pow(pl[i % nl], pb[i % nb] + 1) ) );
+      pans[i] = comp1 + comp2;
+    } else {
+      comp2 = pc[i % nc] * log( log(py2[i % ny]) - log(py1[i % ny]) );
+      comp1 = - pc[i % nc] * log( log(pu[i % nu]) - log(pl[i % nl]) );
+      pans[i] = comp1 + comp2;
+    }
+  }
+
+  UNPROTECT(1);
+  return(ans);
+}
+
+
+/*
+ first derivative of log-likelihood for binned data
+
+ ll_not_minus1 = expression(- c * log( abs( u^(b + 1) - l^(b + 1) ) ) + c * log( abs( y2^(b + 1) - y1^(b + 1) ) ))
+ Deriv::Deriv(ll_not_minus1, "b")
+
+ ll_minus1 = expression( - c * log( log(u) - log(l) ) + c * log( log(y2) - log(y1) ) )
+ Deriv::Deriv(ll_minus1, "b")
+ */
+SEXP score_bpl_binned(SEXP y1, SEXP y2, SEXP b, SEXP c, SEXP l, SEXP u)
+{
+  int ny = LENGTH(y1), nb = LENGTH(b), nc = LENGTH(c), nl = LENGTH(l), nu = LENGTH(u);
+  int N = MAX5(ny, nb, nc, nl, nu);
+
+  SEXP ans;
+  PROTECT(ans = Rf_allocVector(REALSXP, N));
+
+  double e1, e2, e3, e4, e5, e6, e7;
+  double *py1 = REAL(y1), *py2 = REAL(y2), *pb = REAL(b), *pc = REAL(c), *pl = REAL(l), *pu = REAL(u), *pans = REAL(ans);
+
+  for(int i = 0; i < N; i++) {
+    if(pb[i % nb] != -1) {
+      e1 = pb[i % nb] + 1;
+      e2 = R_pow(pl[i % nl], e1);
+      e3 = R_pow(pu[i % nu], e1);
+      e4 = R_pow(py1[i % ny], e1);
+      e5 = R_pow(py2[i % ny], e1);
+      e6 = e3 - e2;
+      e7 = e5 - e4;
+      pans[i] = pc[i % nc] * (sign(e7) * (e5 * log(py2[i % ny]) - e4 * log(py1[i % ny]))/fabs(e7) - sign(e6) * (e3 * log(pu[i % nu]) - e2 * log(pl[i % nl]))/fabs(e6));
+    } else {
+      pans[i] = 0;
     }
   }
 
@@ -40,24 +108,26 @@ SEXP d_bpl(SEXP y, SEXP mu, SEXP l, SEXP u)
 
 
 
-// first derivative of ISD log-likelihood
-// computed using Deriv::Deriv(<log-likelihood>)
-SEXP score_bpl(SEXP y, SEXP mu, SEXP c, SEXP l, SEXP u)
+
+
+// first derivative of ISD log-likelihood for counts of individual sizes
+// computed using Deriv::Deriv(<loglik_bpl> for = -1 and != -1, respectively)
+SEXP score_bpl(SEXP y, SEXP b, SEXP c, SEXP l, SEXP u)
 {
-  int ny = LENGTH(y), nmu = LENGTH(mu), nc = LENGTH(c), nl = LENGTH(l), nu = LENGTH(u);
-  int N = MAX5(ny, nmu, nc, nl, nu);
+  int ny = LENGTH(y), nb = LENGTH(b), nc = LENGTH(c), nl = LENGTH(l), nu = LENGTH(u);
+  int N = MAX5(ny, nb, nc, nl, nu);
 
   SEXP ans;
   PROTECT(ans = Rf_allocVector(REALSXP, N));
 
   double e1, e2, e3;
-  double *py = REAL(y), *pc = REAL(c), *pmu = REAL(mu), *pl = REAL(l), *pu = REAL(u), *pans = REAL(ans);
+  double *py = REAL(y), *pc = REAL(c), *pb = REAL(b), *pl = REAL(l), *pu = REAL(u), *pans = REAL(ans);
 
   for(int i = 0; i < N; i++) {
-    if(pmu[i % nmu] != -1) {
-      e1 = 1 + pmu[i % nmu];
-      e2 = pow(pl[i % nl], e1);
-      e3 = pow(pu[i % nu], e1);
+    if(pb[i % nb] != -1) {
+      e1 = 1 + pb[i % nb];
+      e2 = R_pow(pl[i % nl], e1);
+      e3 = R_pow(pu[i % nu], e1);
       pans[i] = pc[i % nc] * ((1 - e1 * (e3 * log(pu[i % nu]) - e2 * log(pl[i % nl]))/(e3 - e2))/e1 + log(py[i % ny]));
     } else {
       pans[i] = pc[i % nc] * log(py[i % ny]);
@@ -69,31 +139,77 @@ SEXP score_bpl(SEXP y, SEXP mu, SEXP c, SEXP l, SEXP u)
 }
 
 
-// second derivative of ISD log-likelihood
-// computed using Deriv::Deriv(Deriv::Deriv(<log-likelihood>))
-SEXP hess_bpl(SEXP y, SEXP mu, SEXP c, SEXP l, SEXP u)
+/*
+ * second derivative of ISD log-likelihood for binned data
+ * Deriv::Deriv(ll_not_minus1, "b", nderiv = 2)
+ * Deriv::Deriv(ll_minus1, "b", nderiv = 2)
+ */
+SEXP hess_bpl_binned(SEXP y1, SEXP y2, SEXP b, SEXP c, SEXP l, SEXP u)
 {
-  int ny = LENGTH(y), nmu = LENGTH(mu), nc = LENGTH(c), nl = LENGTH(l), nu = LENGTH(u);
-  int N = MAX5(ny, nmu, nc, nl, nu);
+  int ny = LENGTH(y1), nb = LENGTH(b), nc = LENGTH(c), nl = LENGTH(l), nu = LENGTH(u);
+  int N = MAX5(ny, nb, nc, nl, nu);
+
+  SEXP ans;
+  PROTECT(ans = Rf_allocVector(REALSXP, N));
+
+  double e1, e2, e3, e4, e5, e6, e7, e8, e9, e10, e11, e12, e13;
+  double *py1 = REAL(y1), *py2 = REAL(y2), *pb = REAL(b), *pc = REAL(c), *pl = REAL(l), *pu = REAL(u), *pans = REAL(ans);
+
+  for(int i = 0; i < N; i++) {
+    if(pb[i % nb] != -1) {
+      e1 = pb[i % nb] + 1;
+      e2 = R_pow(pl[i % nl], e1);
+      e3 = R_pow(pu[i % nu], e1);
+      e4 = R_pow(py1[i % ny], e1);
+      e5 = R_pow(py2[i % ny], e1);
+      e6 = e3 - e2;
+      e7 = e5 - e4;
+      e8 = log(pl[i % nl]);
+      e9 = log(pu[i % nu]);
+      e10 = log(py1[i % ny]);
+      e11 = log(py2[i % ny]);
+      e12 = sign(e6);
+      e13 = sign(e7);
+
+      pans[i] = pc[i % nc] * (((e5 * R_pow(e11, 2) - e4 * R_pow(e10, 2))/fabs(e7) - e13 * R_pow(e5 * e11 - e4 * e10, 2)/R_pow(e7, 2)) * e13 - ((e3 * R_pow(e9, 2) - e2 * R_pow(e8, 2))/fabs(e6) - e12 * R_pow(e3 * e9 - e2 * e8, 2)/R_pow(e6, 2)) * e12);
+
+      pans[i] = -pans[i]; //return negative second derivative for 'gamlss2'
+
+    } else {
+      pans[i] = 0;
+    }
+  }
+
+  UNPROTECT(1);
+  return(ans);
+}
+
+
+// second derivative of ISD log-likelihood for counts of individual sizes
+// computed using Deriv::Deriv(Deriv::Deriv(<loglik_bpl> for = -1 and != -1, respectively))
+SEXP hess_bpl(SEXP y, SEXP b, SEXP c, SEXP l, SEXP u)
+{
+  int ny = LENGTH(y), nb = LENGTH(b), nc = LENGTH(c), nl = LENGTH(l), nu = LENGTH(u);
+  int N = MAX5(ny, nb, nc, nl, nu);
 
   SEXP ans;
   PROTECT(ans = Rf_allocVector(REALSXP, N));
 
   double e1, e2, e3, e4, e5, e6, e7, e8, e9;
-  double *py = REAL(y), *pc = REAL(c), *pmu = REAL(mu), *pl = REAL(l), *pu = REAL(u), *pans = REAL(ans);
+  double *py = REAL(y), *pc = REAL(c), *pb = REAL(b), *pl = REAL(l), *pu = REAL(u), *pans = REAL(ans);
 
   for(int i = 0; i < N; i++) {
-    if(pmu[i % nmu] != -1) {
-      e1 = 1 + pmu[i % nmu];
-      e2 = pow(pl[i % nl], e1);
-      e3 = pow(pu[i % nu], e1);
+    if(pb[i % nb] != -1) {
+      e1 = 1 + pb[i % nb];
+      e2 = R_pow(pl[i % nl], e1);
+      e3 = R_pow(pu[i % nu], e1);
       e4 = log(pl[i % nl]);
       e5 = log(pu[i % nu]);
       e6 = e2 * e4;
       e7 = e3 - e2;
       e8 = e3 * e5;
       e9 = e8 - e6;
-      pans[i] = -(pc[i % nc] * ((e1 * (e3 * pow(e5, 2) - (pow(e9, 2)/e7 + e2 * pow(e4, 2))) + e8 - e6)/e7 + (1 - e1 * e9/e7)/e1)/e1);
+      pans[i] = -(pc[i % nc] * ((e1 * (e3 * R_pow(e5, 2) - (R_pow(e9, 2)/e7 + e2 * R_pow(e4, 2))) + e8 - e6)/e7 + (1 - e1 * e9/e7)/e1)/e1);
 
       pans[i] = -pans[i]; //return negative second derivative
     } else {
@@ -106,24 +222,61 @@ SEXP hess_bpl(SEXP y, SEXP mu, SEXP c, SEXP l, SEXP u)
 }
 
 
-// log-likelihood; adapted from Edwards et al. 2017 (from 'sizeSpectra' code)
-SEXP loglik_bpl(SEXP y, SEXP mu, SEXP c, SEXP l, SEXP u)
+
+
+/*
+// PDF of ISD; adapted from 'isdbayes' package
+SEXP d_bpl(SEXP y, SEXP b, SEXP l, SEXP u)
 {
-  int ny = LENGTH(y), nmu = LENGTH(mu), nc = LENGTH(c), nl = LENGTH(l), nu = LENGTH(u);
-  int N = MAX5(ny, nmu, nc, nl, nu);
+  int ny = LENGTH(y), nb = LENGTH(b), nl = LENGTH(l), nu = LENGTH(u);
+  int N = MAX4(ny, nb, nl, nu);
 
   SEXP ans;
   PROTECT(ans = Rf_allocVector(REALSXP, N));
 
-  double *py = REAL(y), *pc = REAL(c), *pmu = REAL(mu), *pl = REAL(l), *pu = REAL(u), *pans = REAL(ans);
+  double *py = REAL(y), *pb = REAL(b), *pl = REAL(l), *pu = REAL(u), *pans = REAL(ans);
 
   for(int i = 0; i < N; i++) {
-    if(pmu[i % nmu] != -1) {
-      pans[i] = pc[i % nc] * log((pmu[i % nmu] + 1)/(pow(pu[i % nu], pmu[i % nmu] + 1) - pow(pl[i % nl], pmu[i % nmu] + 1))) + pmu[i % nmu] * pc[i % nc] * log(py[i % ny]);
-    } else {
-      pans[i] = -pc[i % nc] * log(log(pu[i % nu]) - log(pl[i % nl])) - pc[i % nc] * log(py[i % ny]);
-      //pans[i] = pc[i % nc] * (log(log(pl[i % nl]) - log(pu[i % nu])) + pmu[i % nmu] * log(py[i % ny]));
+    if((py[i % ny] < pl[i % nl]) || (py[i % ny] > pu[i % nu])) {
+      pans[i] = 0.0;
+      continue;
     }
+    if(pb[i % nb] != -1) {
+      pans[i] = (pb[i % nb] + 1) * R_pow(py[i % ny], pb[i % nb] + 1) / ( R_pow(pu[i % nu], pb[i % nb] + 1) - R_pow(pl[i % nl], pb[i % nb] + 1) );
+    } else {
+      pans[i] = R_pow(py[i % ny], -2)/(pl[i % nl] * log(pu[i % nu]/pl[i % nl]));
+    }
+  }
+
+  UNPROTECT(1);
+  return(ans);
+}
+*/
+
+// adapted from sizeSpectra::dPLB
+SEXP d_bpl(SEXP y, SEXP b, SEXP l, SEXP u)
+{
+  int ny = LENGTH(y), nb = LENGTH(b), nl = LENGTH(l), nu = LENGTH(u);
+  int N = MAX4(ny, nb, nl, nu);
+
+  SEXP ans;
+  PROTECT(ans = Rf_allocVector(REALSXP, N));
+
+  double *py = REAL(y), *pb = REAL(b), *pl = REAL(l), *pu = REAL(u), *pans = REAL(ans);
+  double C;
+
+  for(int i = 0; i < N; i++) {
+    if((py[i % ny] < pl[i % nl]) || (py[i % ny] > pu[i % nu])) {
+      pans[i] = 0.0;
+      continue;
+    }
+    if(pb[i % nb] != -1) {
+      C = (pb[i % nb] + 1) / ( R_pow(pu[i % nu], pb[i % nb] + 1) - R_pow(pl[i % nl], pb[i % nb] + 1) );pans[i] = (pb[i % nb] + 1) * R_pow(py[i % ny], pb[i % nb] + 1) / ( R_pow(pu[i % nu], pb[i % nb] + 1) - R_pow(pl[i % nl], pb[i % nb] + 1) );
+    } else {
+      C = 1 / (log(pu[i % nu]) - log(pl[i % nl]));
+    }
+
+    pans[i] = C * R_pow(py[i % ny], pb[i % nb]);
   }
 
   UNPROTECT(1);
@@ -131,8 +284,12 @@ SEXP loglik_bpl(SEXP y, SEXP mu, SEXP c, SEXP l, SEXP u)
 }
 
 
+
+
+
+
 /*
- * initialize;
+ * initialize for individual sizes data;
  * adapted from https://github.com/andrew-edwards/sizeSpectra/blob/517c18d84f4326b59807de5235ab4cddac74876b/R/fitting.R#L1507
  */
 SEXP init_bpl(SEXP y, SEXP c, SEXP l, SEXP u)
@@ -160,26 +317,64 @@ SEXP init_bpl(SEXP y, SEXP c, SEXP l, SEXP u)
 }
 
 
-// adapted from 'sizeSpectra'
-SEXP r_bpl(SEXP n, SEXP mu, SEXP l, SEXP u, SEXP rand)
+/*
+ * initialize for binned data
+ *
+ * #https://github.com/andrew-edwards/sizeSpectra/blob/517c18d84f4326b59807de5235ab4cddac74876b/R/simulating.R#L176
+
+   sumCntLogMids = sum(binCounts * log(binMids))
+   #MLEmid (maximum likelihood using midpoints) calculations.
+   #Use analytical value of MLE b for PL model (Box 1, Edwards et al. 2007) as a starting point for nlm for MLE of b for PLB model.
+   PL.bMLE = 1/( log(min(binBreaks)) - sumCntLogMids/sum(binCounts) ) - 1
+ */
+
+SEXP init_bpl_binned(SEXP y1, SEXP y2, SEXP c, SEXP l, SEXP u)
 {
-  int N = INTEGER(n)[0];
-  int nmu = LENGTH(mu), nl = LENGTH(l), nu = LENGTH(u), nrand = LENGTH(rand);
+  int ny = LENGTH(y1), nc = LENGTH(c), nl = LENGTH(l), nu = LENGTH(u);
+  int N = MAX4(ny, nc, nl, nu);
 
   SEXP ans;
   PROTECT(ans = Rf_allocVector(REALSXP, N));
 
-  double *pmu = REAL(mu), *pl = REAL(l), *pu = REAL(u), *prand = REAL(rand), *pans = REAL(ans);
+  double *py1 = REAL(y1), *py2 = REAL(y2), *pc = REAL(c), *pl = REAL(l), *pu = REAL(u), *pans = REAL(ans);
+  double sumc = 0.0, sumclogmids = 0.0;
+
+  for(int i = 0; i < N; i++) {
+    sumc += pc[i % nc];
+    sumclogmids += pc[i % nc] * log( (py1[i % ny] + py2[i % ny]) / 2 );
+  }
+
+  for(int i = 0; i < N; i++) {
+    pans[i] = 1 / ( log(pl[i % nl]) - sumclogmids / sumc ) - 1;
+  }
+
+  UNPROTECT(1);
+  return(ans);
+}
+
+
+
+// adapted from 'sizeSpectra::rPLB'
+// load 'rand' directly from R to avoid messing with random number generation in C
+SEXP r_bpl(SEXP n, SEXP b, SEXP l, SEXP u, SEXP rand)
+{
+  int N = INTEGER(n)[0];
+  int nb = LENGTH(b), nl = LENGTH(l), nu = LENGTH(u), nrand = LENGTH(rand);
+
+  SEXP ans;
+  PROTECT(ans = Rf_allocVector(REALSXP, N));
+
+  double *pb = REAL(b), *pl = REAL(l), *pu = REAL(u), *prand = REAL(rand), *pans = REAL(ans);
 
   for(int i = 0; i < N; i++) {
     if((pl[i % nl] <= 0) || (pl[i % nl] >= pu[i % nu])) {
       pans[i] = NA_REAL;
       continue;
     }
-    if(pmu[i % nmu] != -1) {
-      pans[i] = pow((prand[i % nrand] * pow(pu[i % nu], (pmu[i % nmu] + 1)) + (1 - prand[i % nrand]) * pow(pl[i % nl], (pmu[i % nmu] + 1))), (1/(pmu[i % nmu] + 1)));
+    if(pb[i % nb] != -1) {
+      pans[i] = R_pow((prand[i % nrand] * R_pow(pu[i % nu], (pb[i % nb] + 1)) + (1 - prand[i % nrand]) * R_pow(pl[i % nl], (pb[i % nb] + 1))), (1/(pb[i % nb] + 1)));
     } else {
-      pans[i] = pow(pu[i % nu], prand[i % nrand]) * pow(pl[i % nl], (1 - prand[i % nrand]));
+      pans[i] = R_pow(pu[i % nu], prand[i % nrand]) * R_pow(pl[i % nl], (1 - prand[i % nrand]));
     }
   }
 
@@ -187,26 +382,26 @@ SEXP r_bpl(SEXP n, SEXP mu, SEXP l, SEXP u, SEXP rand)
   return(ans);
 }
 
-// adapted from 'sizeSpectra'
-SEXP q_bpl(SEXP p, SEXP mu, SEXP l, SEXP u)
+// adapted from 'sizeSpectra::qPLB'
+SEXP q_bpl(SEXP p, SEXP b, SEXP l, SEXP u)
 {
-  int np = LENGTH(p), nmu = LENGTH(mu), nl = LENGTH(l), nu = LENGTH(u);
-  int N = MAX4(np, nmu, nl, nu);
+  int np = LENGTH(p), nb = LENGTH(b), nl = LENGTH(l), nu = LENGTH(u);
+  int N = MAX4(np, nb, nl, nu);
 
   SEXP ans;
   PROTECT(ans = Rf_allocVector(REALSXP, N));
 
-  double *pp = REAL(p), *pmu = REAL(mu), *pl = REAL(l), *pu = REAL(u), *pans = REAL(ans);
+  double *pp = REAL(p), *pb = REAL(b), *pl = REAL(l), *pu = REAL(u), *pans = REAL(ans);
 
   for(int i = 0; i < N; i++) {
     if((pl[i % nl] <= 0) || (pl[i % nl] >= pu[i % nu]) || (pp[i % np] < 0) || (pp[i % np] > 1)) {
       pans[i] = NA_REAL;
       continue;
     }
-    if(pmu[i % nmu] != -1) {
-      pans[i] = pow((pp[i % np] * pow(pu[i % nu], (pmu[i % nmu] + 1)) + (1 - pp[i % np]) * pow(pl[i % nl], (pmu[i % nmu] + 1))), (1/(pmu[i % nmu] + 1)));
+    if(pb[i % nb] != -1) {
+      pans[i] = R_pow((pp[i % np] * R_pow(pu[i % nu], (pb[i % nb] + 1)) + (1 - pp[i % np]) * R_pow(pl[i % nl], (pb[i % nb] + 1))), (1/(pb[i % nb] + 1)));
     } else {
-      pans[i] = pow(pu[i % nu], pp[i % np]) * pow(pl[i % nl], (1 - pp[i % np]));
+      pans[i] = R_pow(pu[i % nu], pp[i % np]) * R_pow(pl[i % nl], (1 - pp[i % np]));
     }
   }
 
@@ -214,16 +409,16 @@ SEXP q_bpl(SEXP p, SEXP mu, SEXP l, SEXP u)
   return(ans);
 }
 
-// adapted from 'sizeSpectra'
-SEXP p_bpl(SEXP y, SEXP mu, SEXP l, SEXP u)
+// adapted from 'sizeSpectra::pPLB'
+SEXP p_bpl(SEXP y, SEXP b, SEXP l, SEXP u)
 {
-  int ny = LENGTH(y), nmu = LENGTH(mu), nl = LENGTH(l), nu = LENGTH(u);
-  int N = MAX4(ny, nmu, nl, nu);
+  int ny = LENGTH(y), nb = LENGTH(b), nl = LENGTH(l), nu = LENGTH(u);
+  int N = MAX4(ny, nb, nl, nu);
 
   SEXP ans;
   PROTECT(ans = Rf_allocVector(REALSXP, N));
 
-  double *py = REAL(y), *pmu = REAL(mu), *pl = REAL(l), *pu = REAL(u), *pans = REAL(ans);
+  double *py = REAL(y), *pb = REAL(b), *pl = REAL(l), *pu = REAL(u), *pans = REAL(ans);
   double xmintobplus1, denom, logxmin;
 
   for(int i = 0; i < N; i++) {
@@ -242,10 +437,10 @@ SEXP p_bpl(SEXP y, SEXP mu, SEXP l, SEXP u)
       continue;
     }
 
-    if(pmu[i % nmu] != -1) {
-      xmintobplus1 = pow(pl[i % nl], (pmu[i % nmu] + 1));
-      denom = pow(pu[i % nu], (pmu[i % nmu] + 1)) - xmintobplus1;
-      pans[i] = (pow(py[i % ny], pmu[i % nmu] + 1) - xmintobplus1) / denom;
+    if(pb[i % nb] != -1) {
+      xmintobplus1 = R_pow(pl[i % nl], (pb[i % nb] + 1));
+      denom = R_pow(pu[i % nu], (pb[i % nb] + 1)) - xmintobplus1;
+      pans[i] = (R_pow(py[i % ny], pb[i % nb] + 1) - xmintobplus1) / denom;
     } else {
       logxmin = log(pl[i % nl]);
       denom = log(pu[i % nu]) - logxmin;
@@ -256,6 +451,49 @@ SEXP p_bpl(SEXP y, SEXP mu, SEXP l, SEXP u)
   UNPROTECT(1);
   return(ans);
 }
+
+/*
+ * adapted from
+ *  https://www.int-res.com/articles/suppl/m636p019_supp1.pdf
+ */
+SEXP p_bpl_binned(SEXP y1, SEXP y2, SEXP b, SEXP l, SEXP u)
+{
+  int ny = LENGTH(y1), nb = LENGTH(b), nl = LENGTH(l), nu = LENGTH(u);
+  int N = MAX4(ny, nb, nl, nu);
+
+  SEXP ans;
+  PROTECT(ans = Rf_allocVector(REALSXP, N));
+
+  double *py1 = REAL(y1), *py2 = REAL(y2), *pb = REAL(b), *pl = REAL(l), *pu = REAL(u), *pans = REAL(ans);
+  double xmintobplus1, denom, logxmin;
+
+  for(int i = 0; i < N; i++) {
+    if((pl[i % nl] <= 0) || (pl[i % nl] >= pu[i % nu])) {
+      pans[i] = NA_REAL;
+      continue;
+    }
+
+    if(py1[i % ny] < pl[i % nl]) {
+      pans[i] = 0;
+      continue;
+    }
+
+    if(py2[i % ny] > pu[i % nu]) {
+      pans[i] = 1;
+      continue;
+    }
+
+    if(pb[i % nb] != -1) {
+      pans[i] = ( R_pow(py2[i % ny], pb[i % nb] + 1) - R_pow(py1[i % ny], pb[i % nb] + 1) ) / ( R_pow(pu[i % nu], pb[i % nb] + 1) - R_pow(pl[i % nl], pb[i % nb] + 1) );
+    } else {
+      pans[i] = ( log(py2[i % ny]) - log(py1[i % ny]) ) / ( log(pu[i % nu]) - log(pl[i % nl]) );
+    }
+  }
+
+  UNPROTECT(1);
+  return(ans);
+}
+
 
 
 
